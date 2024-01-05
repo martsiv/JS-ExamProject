0dce4a224e46bc0fe1dda7fc7e0600091ed8584b
Mykola <mykola.martsiv@gmail.com>
2024-01-05 14:24:58 +0200

    Revert to a previous version

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-import MemoryGame from "./JS/memoryGame.js"; 
-import {imagePaths} from "./JS/imagePaths.js"; 
+let imagePaths = [];
+imagePaths.push('./Cards/10-de-copas.png');
+imagePaths.push('./Cards/200px-Playing_card_diamond_9.svg.png');
+imagePaths.push('./Cards/200px-Playing_card_heart_6.svg.png');
+imagePaths.push('./Cards/200px-Playing_card_heart_7.svg.png');
+imagePaths.push('./Cards/819px-Playing_card_club_6.svg.png');
+imagePaths.push('./Cards/1200px-Playing_card_club_8.svg.png');
+imagePaths.push('./Cards/1200px-Playing_card_spade_6.svg.png');
+imagePaths.push('./Cards/09462a668a4c8b5126d0c64c1c42169f.jpg');
+imagePaths.push('./Cards/190996-1024__12779.jpg');
+imagePaths.push('./Cards/32651166-jack-of-diamonds-playing-card.jpg');
+imagePaths.push('./Cards/45128575-poker-playing-card-9-club.jpg');
+imagePaths.push('./Cards/45128581-poker-playing-card-9-heart.jpg');
+imagePaths.push('./Cards/ace-diamonds-playing-card-isolated-white-ace-diamonds-playing-card-isolated-white-clipping-path-included-172707789.jpg');
+imagePaths.push('./Cards/ace-hearts-deck-goodall-son-ltd-14968582.jpg.webp');
+imagePaths.push('./Cards/ace-spades-playing-card-isolated-white-ace-spades-playing-card-isolated-white-clipping-path-included-172550077.jpg');
+imagePaths.push('./Cards/f1db2cddb90fd324cc32cefe1fa066d3.jpg');
+imagePaths.push('./Cards/game-card-6357341.jpg');
+imagePaths.push('./Cards/images.png');
+imagePaths.push('./Cards/imgbin-gong-zhu-queen-of-clubs-playing-card-suit-queen-kZexwNV8y9sDK8VnBhf8b80yM.jpg');
+imagePaths.push('./Cards/imgbin-queen-of-hearts-playing-card-king-playing-cards-queen-of-hearts-playing-card-k3y46UzpkrWbts03BmdnWTpCt.jpg');
+imagePaths.push('./Cards/istockphoto-502944230-612x612.jpg');
+imagePaths.push('./Cards/jack-clubs-playing-card-isolated-white-clipping-path-included-jack-clubs-playing-card-isolated-white-173562459.jpg');
+imagePaths.push('./Cards/king-of-diamonds-card-AERYA3.jpg');
 
 const $gameBodyGrid = $("#gameBody");
 const $generalOverlay = $('#overlay');
@@ -12,10 +34,48 @@ const $timer = $('#timer');
 let currentLvl = parseInt($('#levelSettings .settingActiveBtn').attr('gameLvl'));
 let currentSpeed = parseInt($('#speedSettings .settingActiveBtn').attr('speed'));
 
+// Event for setting actual game level
+$('#levelSettings .settingBtn').on('click', function () {
+  let gameLvlValue = $(this).attr('gameLvl');
+  currentLvl = parseInt(gameLvlValue);
+  $('#levelSettings .settingBtn').removeClass('settingActiveBtn');
+  $(this).addClass('settingActiveBtn');
+});
+
+// Event for setting actual speed
+$('#speedSettings .settingBtn').on('click', function  () {
+  let gameLvlValue = $(this).attr('speed');
+  currentSpeed = parseInt(gameLvlValue);
+  $('#speedSettings .settingBtn').removeClass('settingActiveBtn');
+  $(this).addClass('settingActiveBtn');
+});
+
 let timerInterval;
 let openedCards = [];
 let imagesForGame = [];
 let guessedPairs = 0;
+
+
+class MemoryGame {
+  constructor() {
+    this._counterMoves = 0;
+    this._counterMistakes = 0;
+  }
+  get counterMoves() {
+    return this._counterMoves;
+  }
+  set counterMoves(value) {
+    this._counterMoves = value;
+    $moves.text(this._counterMoves);
+  }
+  get counterMistakes() {
+    return this._counterMistakes;
+  }
+  set counterMistakes(value) {
+    this._counterMistakes = value;
+    $mistakes.text(this._counterMistakes);
+  }
+}
 let gameObject;
 
 
@@ -41,21 +101,6 @@ function shuffleImagePaths(array) {
   return shuffledArray;
 }
 
-// Event for setting actual game level
-$('#levelSettings .settingBtn').on('click', () => {
-  let gameLvlValue = $(this).attr('gameLvl');
-  currentLvl = parseInt(gameLvlValue);
-  $('#levelSettings .settingBtn').removeClass('settingActiveBtn');
-  $(this).addClass('settingActiveBtn');
-});
-
-// Event for setting actual speed
-$('#speedSettings .settingBtn').on('click', () => {
-  let gameLvlValue = $(this).attr('speed');
-  currentSpeed = parseInt(gameLvlValue);
-  $('#speedSettings .settingBtn').removeClass('settingActiveBtn');
-  $(this).addClass('settingActiveBtn');
-});
 
 // In depends on game level selector this function generates a game grid with the appropriate numbers of cells
 function generateGameGrid() {
@@ -86,7 +131,7 @@ function prepareImgForRound(size) {
 // -------------------------------------------------------------------------------------------------------------------
 
 // START GAME BUTTON
-$("#overlayButton").on('click', () => {
+$("#overlayButton").on('click', function () {
   clearDataBeforeNewGame();
   $generalOverlay.css('display', 'none');
   $overlayBtn.hide();
